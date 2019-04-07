f4bc55a1e479079aa4e0fe4357a1daaf2562a0ee
Mike Zasch <m.caceffo@gmail.com>
2019-04-07 16:03:46 +0200

    Added Triggers definition

--- P1_Q2-b.sql
+++ P1_Q2-b.sql
@@ -1,4 +1,20 @@
 Attore(Id, Nome, Cognome)
-Film(Id, Titolo, Anno, Genere, Trailer, Trama, Durata)
-Utente(CF, Username, Password)
+Film(Id, Titolo, Anno, Genere, Trailer, Trama, Durata[, nVisualizzazioni])
+Utente(CF, Username, Password[, nFilmVisti])
 Visualizza(IdFilm, CFUtente, DataVisualizza)
+
+DELIMITER $$
+
+CREATE TRIGGER IncrementaView
+  AFTER INSERT ON Visualizza FOR EACH ROW
+  BEGIN
+    UPDATE Film SET nVisualizzazioni = nVisualizzazioni + 1
+      WHERE Film.Id = NEW.IdFilm;
+  END$$
+
+CREATE TRIGGER DecrementaView
+  AFTER DELETE ON Visualizza FOR EACH ROW
+  BEGIN
+    UPDATE Film SET nVisualizzazioni = nVisualizzazioni - 1
+      WHERE Film.Id = OLD.IdFilm;
+  END$$
